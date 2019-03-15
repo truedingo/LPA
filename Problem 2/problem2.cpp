51cb4ab37835d3d1d0124d0402987d568fd33760
Pedro Chicória <pedrochicoria@MacBook-Pro-de-Pedro-5.local>
2019-03-15 11:44:00 +0000

    done

--- Problem 2/problem2.cpp
+++ Problem 2/problem2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <stdio.h>
 #include <vector>
+#include <algorithm>
 
 #define MAX_EVENTS 10000
 #define LARGEST_DEADLINE 100000
@@ -9,6 +9,7 @@ using namespace std;
 
 int n_events;
 int events[MAX_EVENTS][3];
+int events_table[MAX_EVENTS][LARGEST_DEADLINE];
 
 
 // A utility function to swap two elements 
@@ -68,6 +69,36 @@ void quickSort( int low, int high)
 } 
 
 
+int maximumProfit(){
+    //int count=0;
+    int max_deadline = events[n_events-1][0];
+    //cout<<"Max Deadline: "<< max_deadline<<endl;
+    for (int t = 0; t < max_deadline; t++){
+        events_table[0][t] = 0;
+    }
+
+    for (int i=1; i < n_events; i++){
+       //cout<<"MAX DEADLINE: "<<events[n_events-1][0]<<endl;
+        for (int t = 0; t< max_deadline; t++){
+            int new_T  = min(t,events[i][0]) - events[i][1]; 
+            if (new_T < 0){
+                events_table[i][t] = events_table[i-1][t];
+            }
+            else{
+                events_table[i][t] = max(events_table[i-1][t],events[i][2]+ events_table[i-1][new_T]);
+                
+            }
+            //cout<<events_table[i][t]<< " ";
+        }
+        //cout<<endl;
+    }
+    //cout<< events_table[n_events-1][events[n_events-1][0]-1]<<endl;
+
+    return events_table[n_events-1][max_deadline-1];
+}
+
+
+
 int main(){
 
     cin>>n_events;
@@ -77,13 +108,13 @@ int main(){
     }
 
     quickSort(0,n_events-1);
+    
+    //cout<<"Number of Events: "<<n_events<<endl;
 
-    cout<<"Number of Events: "<<n_events<<endl;
-
-    for(int event=0;event<n_events;event++){
+    /*for(int event=0;event<n_events;event++){
         cout << "Deadline: " << events[event][0]<< " Duration: " <<events[event][1] << " Cost: " <<events[event][2] << endl;
-    }
+    }*/
 
-
+    cout<<maximumProfit()<<endl;
     return 0;
 }
