69b042f1b60b1e0e3cc22fb9606c4c093dc70d8a
Pedro Chicória <pmgc@student.dei.uc.pt>
2019-05-15 02:02:01 +0100

    Problema L

--- Graphs/Problem L/main.cpp
+++ Graphs/Problem L/main.cpp
@@ -1,55 +1,180 @@
-#include<iostream>
-#include <algorithm>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <vector>
-#include <cstring>
 #include <iostream>
-#include <fstream>
-#include <sstream>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 using namespace std;
 
-#define MAX_CONNECTIONS 1000
+int V;
+int edges;
 
-typedef struct Node{
-  int x,y;
-}Node;
+#define M 751
+int Graph[M][M] = {{0}};
 
-typedef struct Connection{
-  int source, target;
-  double weight;
-}Connection;
-
-int n_nodes;
-int n_connections;
-vector<Node> coordinates_vector;
-Connection connections[MAX_CONNECTIONS];
-
-void calculateWeight(){
-  for(int i=0;i<n_connections;i++){
-    connections[i].weight= (double) sqrt( fabs(coordinates_vector[connections[i].source -1].x -coordinates_vector[connections[i].target -1].x) * fabs(coordinates_vector[connections[i].source -1].x - coordinates_vector[connections[i].target -1].x) + fabs(coordinates_vector[connections[i].source -1].y - coordinates_vector[connections[i].target -1].y) * fabs(coordinates_vector[connections[i].source -1].y - coordinates_vector[connections[i].target -1].y));
-    cout<<connections[i].weight<<endl;
-   
-  }
+void clean_Graph()
+{
+    int i, ii;
+    for (i = 1; i < M; i++)
+    {
+        for (ii = 1; ii < M; ii++)
+        {
+            Graph[i][ii] = 0;
+        }
+    }
 }
 
 
-int main(){
-  while(scanf("%d",&n_nodes)!=EOF){
-    for (int i=0;i<n_nodes;i++){
-      Node node;
-      cin>>node.x>>node.y;
-      coordinates_vector.push_back(node);
+double weight(int * x_c, int * y_c, int n1, int n2)
+{
+    return (double) sqrt( (double) fabs(x_c[n1] - x_c[n2]) * (double) fabs(x_c[n1] - x_c[n2]) + (double) fabs(y_c[n1] - y_c[n2]) * (double)fabs(y_c[n1] - y_c[n2]));
+}
+
+struct subset 
+{ 
+    int parent; 
+    int rank; 
+};
+
+struct edge
+{
+    int e, d;
+    double weight;
+};
+   
+int find(struct subset subsets[], int i) 
+{
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+  
+    return subsets[i].parent; 
+}
+
+
+void Union(struct subset subsets[], int x, int y) 
+{ 
+    int xroot = find(subsets, x); 
+    int yroot = find(subsets, y);
+  
+
+    if (subsets[xroot].rank < subsets[yroot].rank) 
+        subsets[xroot].parent = yroot; 
+    else if (subsets[xroot].rank > subsets[yroot].rank) 
+        subsets[yroot].parent = xroot; 
+
+    else
+    { 
+        subsets[yroot].parent = xroot; 
+        subsets[xroot].rank++; 
+    } 
+}
+
+int myComp(const void* a, const void* b) 
+{ 
+    struct edge* a1 = (struct edge*)a; 
+    struct edge* b1 = (struct edge*)b;
+    
+    return a1->weight > b1->weight; 
+} 
+
+
+void KruskalMST(struct edge* E, struct subset * subsets, int num_missing_edges)
+{ 
+     
+    
+    int i = 0;
+    // edges é o nr de edges
+    double c = 0;
+
+    qsort(E, num_missing_edges, sizeof(struct edge), myComp); 
+
+    
+    while (i < num_missing_edges)
+    {
+        
+        struct edge next_edge = E[i++];
+
+        // cout << next_edge.e << " " << next_edge.d << " " << next_edge.weight << endl;
+
+        if (Graph[next_edge.e][next_edge.d] == 0 && Graph[next_edge.d][next_edge.e] == 0)
+        {
+            // cout << "Escolhi " << next_edge.e << " " << next_edge.d << " " << next_edge.weight << endl;
+            int x = find(subsets, next_edge.e);
+            int y = find(subsets, next_edge.d);
+
+            if (x != y)
+            {
+                Union(subsets, x, y);
+                Graph[next_edge.e][next_edge.d] = 1, Graph[next_edge.d][next_edge.e] = 1;
+                c += next_edge.weight;
+            }
+        }
+
     }
-   cin>>n_connections;
-   for(int m=0;m<n_connections;m++){
-     Connection conn;
-      cin>>conn.source>>conn.target;
-      connections[m] = conn;
-      calculateWeight();
-   }
-  
-  }
-  
+
+    printf("%.2lf\n",c); 
+    return; 
+} 
+
+
+int main()
+{
+
+    int n;
+    int e, d;
+    
+    while(cin >> n)
+    {
+        
+        V = n;
+        int x_coord[n], y_coord[n];
+        clean_Graph();
+
+        for (int i = 1; i <= n; ++i)
+        {
+            cin >> x_coord[i], cin >> y_coord[i];
+        }
+
+        
+        struct subset *subsets = (struct subset*) malloc(1010 * sizeof(struct subset) );
+        for (int v = 1; v <= V; ++v)
+        {
+            subsets[v].parent = v; 
+            subsets[v].rank = 0;
+        }
+
+        cin >> edges;
+        struct edge *E = (struct edge*) malloc( 1010* 1010* sizeof(struct edge));
+        int c = 0;
+
+
+        for(int i=1;i<=n;i++)
+		    for(int j=i+1;j<=n;j++)
+			{
+                E[c].e = i;
+                E[c].d = j;
+                E[c].weight = weight(x_coord, y_coord, i, j);
+                c++;
+            }
+    
+        for (int i = 0; i < edges; ++i)
+        {
+            cin >> e, cin >> d;
+
+            Graph[e][d] = 1;
+            Graph[d][e] = 1;
+            
+            // Union the inputed edges
+            int x = find(subsets, e), y = find(subsets, d);
+            
+            if (x != y)
+            {
+                //cout<<"Junta: "<<x<<" com "<<y<<endl;
+                Union(subsets, x, y);
+            }
+        }
+        KruskalMST(E, subsets, c); 
+    }
+
+
+    return 0;
 }
--- Graphs/Problem M/main.cpp
+++ Graphs/Problem M/main.cpp
@@ -11,7 +11,6 @@ struct person{
     int id;
 };
 
-
 int nnodes,nconn;
 vector<vector<int> > graph;
 vector<person> persons;
@@ -21,14 +20,12 @@ vector<vector<int> > init_matrix(int m, int n){
 	vector<vector<int> > matrix(m, vector<int>(n,0));
 	return matrix;
 }
-
 int reid(char name[]){
     for(int i = 0; i < nnodes; i++){
         if(strcmp(name,persons[i].name) == 0){
             return persons[i].id;
         }
     }
-
     return -1;
 }
 
@@ -36,16 +33,13 @@ void dfs(int node, bool vis[])
 { 
     // Mark as visited 
     vis[node] = true; 
-  
     // Traverse for all its children 
     for (int i = 0; i < nnodes; i++) { 
-  
         // If not visited 
         if (graph[node][i] && !vis[i]){
             dfs(i, vis); 
             global++; 
         } 
-  
     } 
 } 
 
@@ -56,7 +50,6 @@ int findLongestPath(int n)
     // has been visited previously or not 
     bool vis[n + 1]; 
     memset(vis, false, sizeof vis); 
-  
     // Call DFS for every unvisited vertex 
     for (int i = 1; i <= n; i++) { 
         if (!vis[i]) 
@@ -66,50 +59,30 @@ int findLongestPath(int n)
             }
             global = 1;
     } 
-  
-  
     // Traverse and find the maximum of all dp[i] 
-
     return aux; 
 } 
 
 int main(){
     char p1[50], p2[50];
     int id = 0,id1,id2;
-
     global = 1;
-
     scanf("%u %u", &nnodes, &nconn);
-
     for (int i = 0; i < nnodes; i++){
         person Person;
-
         scanf("%s",Person.name);
-
-
         Person.id = id;
-
         persons.push_back(Person);
-
         id++;
     }
-
-
     graph = init_matrix(nnodes+1,nnodes+1);
-
     for (int i = 1; i <= nconn; i++){
         scanf("%s %s",p1,p2);
-
         id1 = reid(p1);
         id2 = reid(p2);
-
         graph[id1][id2] = 1;
         graph[id2][id1] = 1;
     }
-
-
     cout << findLongestPath(nnodes) << "\n"; 
-
     return 0;
-
 }
